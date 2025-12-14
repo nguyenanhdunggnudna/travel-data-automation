63041e935ebc394deebb718eeb8d5ae794f85d9f
Anh Dung <nguyenanhdunggnudna2@gmail.com>
2025-12-14 11:02:53 +0700

    refactor code

--- src/modules/broswer/broswer.ts
+++ src/modules/broswer/broswer.ts
@@ -11,7 +11,15 @@ export class BrowserService {
 
     this.browser = await puppeteer.launch({
       headless: headless,
-      args: ['--no-sandbox', '--disable-setuid-sandbox']
+      args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage',
+        '--disable-accelerated-2d-canvas',
+        '--no-zygote',
+        '--single-process',
+        '--disable-gpu'
+      ]
     });
 
     return this.browser;
